a308597b0cfa9aba3ec7449fb2672fd81c1eb0e1
unknown <el.zwierzakko@gmail.com>
2020-09-30 17:36:06 +0200

    style and basic code changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 const api = {
   key: "a6a955cc0efd6e6ef67a699dad70fe38",
   base: "https://api.openweathermap.org/data/2.5/",
 }
 
 function App() {
+
+  const [query, setQuery] = useState('');
 
   const DateBuilder = (d) => {
     let months = ["Styczeń", "Luty", "Marzec", "Kwiecień", "Maj", "Czerwiec", "Lipiec", "Sierpień", "Wrzesień", "Październik", "Listopad", "Grudzień"]
@@ -26,6 +28,14 @@ function App() {
      <div className="location-box">
        <div className="location">Warszawa, PL</div>
   <div className="date">{DateBuilder(new Date())}</div>
+     </div>
+     <div className="weather-box">
+       <div className="temp">
+         15 °C
+       </div>
+       <div className="weather">
+         Sunny
+       </div>
      </div>
       </main>
     </div>
